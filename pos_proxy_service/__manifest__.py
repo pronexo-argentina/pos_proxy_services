04e2b6d97f9d8b5b8ba77f613037d1e18cfd9463
Pronexo.com <pronexo@gmail.com>
2021-12-06 19:58:48 -0300

    Update __manifest__.py

--- pos_proxy_service/__manifest__.py
+++ pos_proxy_service/__manifest__.py
@@ -45,7 +45,7 @@
     },
 	'auto_install': False,
 	'installable': True,
-        'price': 60,
+        'price': 0,
         'currency': 'USD',
         'images': ['images/pos-proxy-service-home.png'],
         'live_test_url': 'https://www.youtube.com/watch?v=SKFlc8bKZAI'
