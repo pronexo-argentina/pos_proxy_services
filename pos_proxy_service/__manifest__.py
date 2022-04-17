10147e06cc9fdc1edf328709044423883533a92d
Pronexo.com <pronexo@gmail.com>
2022-04-17 01:16:07 -0300

    Update __manifest__.py

--- pos_proxy_service/__manifest__.py
+++ pos_proxy_service/__manifest__.py
@@ -21,7 +21,7 @@
 	'name': 'Pos Proxy Services',
 	'summary': 'Proxy para usar odoo con impresores fiscales Argentinos para Epson / Hasar',
 	'description': 'Impresor Fiscal Epson, Impresor Fiscal Hasar Vieja y Nueva generacion',
-	'version': '14.0.2.1',
+	'version': '14.0.2.2',
 	'author': "Pronexo",
 	'license': "AGPL-3",
 	'maintainer': 'Pronexo',
@@ -45,7 +45,7 @@
     },
 	'auto_install': False,
 	'installable': True,
-	'price': 280,
+	'price': 0,
         'currency': 'USD',
         'images': ['images/pos-proxy-service-home.png'],
         'live_test_url': 'https://www.youtube.com/watch?v=SKFlc8bKZAI'
